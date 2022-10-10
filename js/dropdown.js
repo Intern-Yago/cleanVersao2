2064f9b7f414f3cd51ddb5a8ea1b4258d8de50a6
Intern-Yago <yagovictorbotafogo@gmail.com>
2022-10-10 06:20:29 -0300

    bestclean

--- js/dropdown.js
+++ js/dropdown.js
@@ -10,12 +10,3 @@ const showMenu = (toggleId, navId)=>{
   }
 }
 showMenu('header-toggle','nav-menu')
-
-const showCont = (id, toggle_id) =>{
-  const element = document.getElementById(id),
-  toggle = document.getElementById(toggle_id)
-  if(window.innerWidth <= 900){
-    element.classList.toggle('show')
-    toggle.classList.toggle('bx-x')
-  }
-}
