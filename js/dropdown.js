a678aa8794571248a94ce74c149bb79ecf64258a
Intern-Yago <yagovictorbotafogo@gmail.com>
2022-10-11 16:20:13 -0300

    bestclean

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,12 +1,25 @@
-const showMenu = (toggleId, navId)=>{
+const handleShow = (nav, toggle)=>{
+  nav.classList.toggle('show')
+  toggle.classList.toggle('bx-x')
+}
+
+const showMenu = (toggleId, navId, buttonsClass)=>{
   const toggle = document.getElementById(toggleId),
-  nav = document.getElementById(navId)
+  nav = document.getElementById(navId),
+  buttonsHtml = document.getElementsByClassName(buttonsClass)
+  var buttons = [...buttonsHtml]
 
   if(toggle && nav){
+    buttons.map(button=>{
+      button.addEventListener('click', ()=>{
+        handleShow(nav, toggle)
+      })
+    })
     toggle.addEventListener('click', ()=>{
-      nav.classList.toggle('show')
-      toggle.classList.toggle('bx-x')
+      handleShow(nav, toggle)
     })
   }
 }
-showMenu('header-toggle','nav-menu')
+
+showMenu('header-toggle','nav-menu', 'nav_button')
+
